8a2179d34fc44d03cdba0261cf91f16555d4ea0a
JasonXBlue <JasonX.blue1@gmail.com>
2020-05-02 13:53:03 -0500

    add login

--- BlankSignOn/src/app/login/login.component.ts
+++ BlankSignOn/src/app/login/login.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get passwordValid(): boolean {
+    return (
+      !this.loginForm.controls.inputPassword.valid &&
+      this.loginForm.controls.inputPassword.touched
+    );
+  }
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      inputEmail: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.email,
+          Validators.maxLength(100),
+        ]),
+      ],
+      inputPassword: [
+        '',
+        Validators.compose([Validators.required, Validators.maxLength(30)]),
+      ],
+    });
   }
 
+  ngOnInit(): void {}
+
+  submit() {
+    console.log(this.loginForm);
+  }
 }
